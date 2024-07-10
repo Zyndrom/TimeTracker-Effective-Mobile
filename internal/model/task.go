70f2b33505c9291220c0fc063c9a84ab1a26d891
Zyndram <den_deniskin@inbox.ru>
2024-07-10 16:09:01 +0300

    Update model example

--- internal/model/task.go
+++ internal/model/task.go
@@ -5,7 +5,7 @@ import "time"
 type Task struct {
 	Id        int       `json:"id" example:"1"`
 	Owner     User      `json:"user"`
-	Name      string    `json:"name" example:"string"`
+	Name      string    `json:"name" example:"Example"`
 	CreatedAt time.Time `json:"created_at" example:"2024-07-09T18:15:32.579945Z"`
 	UpdatedAt time.Time `json:"updated_at" example:"2024-07-09T18:15:32.579945Z"`
 	IsActive  bool      `json:"is_active" example:"true"`
--- internal/model/user.go
+++ internal/model/user.go
@@ -3,8 +3,8 @@ package model
 type User struct {
 	Id             int    `json:"id" example:"1"`
 	PassportNumber string `json:"passportNumber" example:"1234 567890"`
-	Name           string `json:"name" example:"string"`
-	Surname        string `json:"surname" example:"string"`
-	Patronymic     string `json:"patronymic,omitempty" example:"string"`
-	Address        string `json:"address" example:"string"`
+	Name           string `json:"name" example:"Petr"`
+	Surname        string `json:"surname" example:"Petr"`
+	Patronymic     string `json:"patronymic,omitempty" example:"Petr"`
+	Address        string `json:"address" example:"Piter"`
 } // @name User
